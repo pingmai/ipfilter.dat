d1c066ed07a37a1e390cc2b25f2a7a927d8dce82
ping <ping@stepnet.com>
2018-08-16 10:48:03 -0700

    checkin

--- iblocklist.py
+++ iblocklist.py
@@ -1,4 +1,57 @@
 #!/usr/local/bin/python3.7
-import urllist.request
+import argparse
 import gzip
+import sys
+import urllib.request
+import ipaddress
 
+parser = argparse.ArgumentParser(
+    description='''download block list from iblocklist.com
+    convert to cidr addresses.
+    ''',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument(
+    '-i',
+    '--infile',
+    default='lists.input',
+    help='input file with urls of list of lists'
+)
+parser.add_argument(
+    '-o',
+    '--outfile',
+    default='ipfilter.dat',
+    help='output file'
+)
+args = parser.parse_args()
+
+nets = set()
+for line in open(args.infile, 'r'):
+    url = line.split('#')[0].strip()
+    if not url:
+        continue
+    try:
+        response = urllib.request.urlopen(url)
+    except Exception as e:
+        print('%s: ' % url, e)
+    else:
+        print(url)
+        for line in gzip.open(response):
+            line = line.decode(errors='ignore').strip()
+            a = line.rsplit(':',1)
+            if len(a) != 2:
+                print('skipped:', line)
+                continue
+            comment, range = a
+            a = range.split('-')
+            if len(a) != 2:
+                print('bad ip range:', line)
+                continue
+            start,end = map(ipaddress.IPv4Address, a)
+            nets.update(list(ipaddress.summarize_address_range(start, end)))
+
+outf = open(args.outfile,'w')
+for n in ipaddress.collapse_addresses(nets):
+    print("%s - %s" % (n[0],n[-1]), file=outf)
+
+outf.close()
